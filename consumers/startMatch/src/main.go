505a3b16d2712465e3f64fb7f6978fed49acde0c
axm <amocanu2000@gmail.com>
2019-08-30 23:56:41 +0100

    Updates to consumers

--- consumers/startMatch/src/main.go
+++ consumers/startMatch/src/main.go
@@ -7,6 +7,10 @@ import (
 	"github.com/axm/apollo-utils"
 	"github.com/pkg/errors"
 	"github.com/streadway/amqp"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
 )
 
 const (
@@ -16,36 +20,95 @@ const (
 type startMatch struct {
 }
 
-func main() {
-	dc := &apollo.DatabaseConnection{
-		Server:     "",
-		Port:       0,
-		UserId:     "",
-		Password:   "",
-		Database:   "",
-		DriverName: "",
-	}
+type Config map[string]*json.RawMessage
 
-	cs := &apollo.RabbitConsumerSettings{
-		Queue:     "",
-		Consumer:  "",
-		AutoAck:   false,
-		Exclusive: false,
-		NoLocal:   false,
-		NoWait:    false,
-		Args:      nil,
-	}
-
-	rc := &apollo.RabbitConnection{
-		User:     "",
-		Password: "",
-		Host:     "",
-		Port:     0,
-	}
-
-	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%d", rc.User, rc.Password, rc.Host, rc.Port))
+func readFile(relativePath string) ([]byte, error) {
+	cwd, err := os.Getwd()
+	fmt.Println(fmt.Sprintf("cwd: %s", cwd))
 	if err != nil {
-		fmt.Println(err)
+		return nil, errors.Wrap(err, "Unable to get cwd.")
+	}
+	path := path.Join(cwd, relativePath)
+	fmt.Println(fmt.Sprintf("Path: %s", path))
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to read file.")
+	}
+
+	return contents, nil
+}
+
+func readDatabaseConnection(config *Config) (*apollo.DatabaseConnection, error) {
+	var dc apollo.DatabaseConnection
+	bytes, err := (*config)["Database"].MarshalJSON()
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to read database settings section")
+	}
+	err = json.Unmarshal(bytes, &dc)
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to parse database settings")
+	}
+
+	return &dc, nil
+}
+
+func readRabbitSettings(config *Config) (*apollo.RabbitConnection, *apollo.RabbitConsumerSettings, error) {
+	buffer, err := (*config)["Rabbit"].MarshalJSON()
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "Unable to read rabbit settings section.")
+	}
+
+	rabbitMap := make(Config)
+	err = json.Unmarshal(buffer, &rabbitMap)
+
+	buffer, err = rabbitMap["Connection"].MarshalJSON()
+	var rc apollo.RabbitConnection
+	err = json.Unmarshal(buffer, &rc)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "Unable to parse rabbit connection settings.")
+	}
+
+	var cs apollo.RabbitConsumerSettings
+	buffer, err = rabbitMap["Consumer"].MarshalJSON()
+	err = json.Unmarshal(buffer, &cs)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "Unable to parse rabbit consumer settings.")
+	}
+
+	return &rc, &cs, nil
+}
+
+func main() {
+	// configContents, err := readFile("consumers/startMatch/src/config.json")
+	configContents, err := readFile("config.json")
+	if err != nil {
+		log.Fatal("Error reading config file. Aborting.")
+		return
+	}
+
+	jsonConfig := make(Config)
+	err = json.Unmarshal(configContents, &jsonConfig)
+	if err != nil {
+		log.Fatal("Error parsing config. Aborting.")
+		return
+	}
+
+	dc, err := readDatabaseConnection(&jsonConfig)
+	if err != nil || dc == nil {
+		log.Fatal("Error reading database connection config")
+		return
+	}
+
+	rc, cs, err := readRabbitSettings(&jsonConfig)
+	if err != nil {
+		log.Fatal("Error reading rabbit connection config")
+	}
+
+	rabbitConnectionString := fmt.Sprintf("amqp://%s:%s@%s:%d", rc.User, rc.Password, rc.Host, rc.Port)
+	conn, err := amqp.Dial(rabbitConnectionString)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Rabbit connection string: %s", rabbitConnectionString))
+		fmt.Println(fmt.Sprintf("Error connecting to rabbit: %s", err))
 		return
 	}
 	defer conn.Close()
@@ -83,8 +146,16 @@ func main() {
 		return
 	}
 
-	whileTrue := make(chan bool)
-	<-whileTrue
+	closeFlag := make(chan bool)
+	consume := func(closeFlag chan bool) {
+		for m := range msgs {
+			fmt.Println(string(m.Body))
+		}
+	}
+
+	go consume(closeFlag)
+	_ = <-closeFlag
+	fmt.Println("Received close flag")
 }
 
 func storeEvent(goal *startMatch, dc *apollo.DatabaseConnection) error {

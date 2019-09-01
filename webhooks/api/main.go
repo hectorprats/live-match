befd07ae86d0693a5536a182c223bf06f733db2e
axm <amocanu2000@gmail.com>
2019-09-01 02:22:06 +0100

    Added boilerplate to webhooks api

--- webhooks/api/main.go
+++ webhooks/api/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
+)
+
+func main() {
+	fmt.Println("Starting webhooks server...")
+	router := mux.NewRouter()
+
+	server := &http.Server{
+		Addr:         ":9091",
+		Handler:      router,
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  10 * time.Second,
+	}
+	server.ListenAndServe()
+}
